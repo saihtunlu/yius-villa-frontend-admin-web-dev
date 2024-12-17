7eedcc10ee4b1437aa671fe86e77432870672ab2
Saihtunlu <saihtunlu14996@gmail.com>
2024-12-17 10:13:39 +0630

    update barcode

--- src/utils/barcodeGenerator.js
+++ src/utils/barcodeGenerator.js
@@ -1,7 +1,9 @@
 const barcodeGenerator = (preFix) => {
   const timestamp = (new Date() / 100).toFixed(0);
+  const randomInt = Math.floor(Math.random() * 10);
+
   const str = `${timestamp}`.substring(timestamp.length - 6);
-  return preFix + str;
+  return preFix + str + randomInt;
 };
 
 export default barcodeGenerator;
